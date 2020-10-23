e21867c9aa7d649816e62bee85defb903f3ed0be
Stefan Morar <stefan.morar30@gmail.com>
2020-10-23 14:56:48 +0300

    2. Express (API: Delete + Read refactor)

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,16 +1,11 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    return res.send(Object.values(req.context.categories));
+    return res.status(200).send(Object.values(req.context.categories));
 });
 
 router.get('/:categoryId/products', (req, res) => {
-    var category = null;
-    Object.values(req.context.categories).forEach(element => {
-        if (element.id === parseInt(req.params.categoryId)) {
-            category = element;
-        }
-    });
+    var category = findCategory(Object.values(req.context.categories), req.params.categoryId);
     if (category !== null) {
         var productsInCategory = []
         Object.values(req.context.products).forEach(element => {
@@ -18,9 +13,19 @@ router.get('/:categoryId/products', (req, res) => {
                 productsInCategory.push(element);
             }
         });
-        return res.send(productsInCategory);
+        return res.status(200).send(productsInCategory);
     }
     res.status(404).send('Category with ID: ' + req.params.categoryId + ' not found.');
 });
+
+function findCategory(categories, categoryId) {
+    var category = null;
+    categories.forEach(element => {
+        if (element.id === parseInt(categoryId)) {
+            category = element;
+        }
+    });
+    return category;
+}
 
 module.exports = router;
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,16 +1,35 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    return res.status(200).send(Object.values(req.context.products));
+});
+
 router.get('/:productId', (req, res) => {
-    var product = null;
-    Object.values(req.context.products).forEach(element => {
-        if (element.id === parseInt(req.params.productId)) {
-            product = element;
-        }
-    });
+    var product = findProduct(Object.values(req.context.products), req.params.productId);
     if (product !== null) {
         return res.status(200).json(product);
     }
     res.status(404).send('Product with ID: ' + req.params.productId + ' not found.');
 });
+
+router.delete('/:productId', (req, res) => {
+    var products = Object.values(req.context.products);
+    var product = findProduct(products, req.params.productId);
+    if (product !== null) {
+        products.splice(Object.values(req.context.products).indexOf(product), 1);
+        return res.status(200).json(product);
+    }
+    res.status(404).send('Product with ID: ' + req.params.productId + ' not found.');
+});
+
+function findProduct(products, productId) {
+    var product = null;
+    products.forEach(element => {
+        if (element.id === parseInt(productId)) {
+            product = element;
+        }
+    });
+    return product;
+}
 
 module.exports = router;
