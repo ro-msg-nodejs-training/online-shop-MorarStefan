34fc021e9581a174aff5761b6562044e13272db2
Stefan Morar <stefan.morar30@gmail.com>
2020-11-23 10:14:17 +0200

    Permit requests from front-end

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (_req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 require("dotenv").config({ path: __dirname + "/../.env" });
 
 require("./database");
@@ -16,4 +24,4 @@ app.use("/suppliers", routes.supplierRoute);
 app.use("/orders", routes.orderRoute);
 app.use("/stocks", routes.stockRoute);
 
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(4000, () => console.log("Listening on port 4000!"));
